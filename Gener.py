b7adfe457415fd7e70905605b2c882accea484af
Ivan Kochegarov <ivankochegarov1@gmail.com>
2017-11-26 04:33:49 +0300

    added Pearson.

--- Gener.py
+++ Gener.py
@@ -6,29 +6,39 @@ class Gen(object):
     e = np.exp(1)
     z = 0
     zmax = e - 1
+    f1 = np.random.random()
 
-    def recursive_function(self):
-        f1 = np.random.random()
+    def function(self,f1):
+        x = 1/(f1+1)
+        return x
+
+
+    def recursive_function(self,f1):
         self.z = pow(self.e, f1) - 1
         return self.z
 
     def array_of_func(self, n):
         array = []
         for i in range(n):
-            array.append(self.recursive_function(self))
+            f1 = np.random.random()
+            array.append(self.recursive_function(self,f1))
         array.sort()
         return array
 
-    def cut(self,l1,l2,x):
+    def cut(self,l1,l2,x,n):
         xone=0
-        xtwo=len(x)
-        for i in range(len(x)):
-            if l1<=x[i]:
-                xone = i
+        xtwo=0
+        i=0
+        j=0
+        while l1 > x[i]:
+            xone = i
+            i += 1
+            if i > n-1:
                 break
-        for j in range(len(x)):
-            if l2<=x[i]:
-                xtwo = x[i-1]
+        while l2 > x[j]:
+            xtwo = j
+            j+=1
+            if j>n-1:
                 break
 
         return xone,xtwo
--- MathO.py
+++ MathO.py
@@ -6,28 +6,58 @@ class Math1(object):
 
     n = 500
     m = 30
+    P=0.01
     deltax = (np.exp(1) - 1) / m
-    x = Gen.array_of_func( Gen, n )
-    v = [0]
-    p = [0]
-    n1=[0]
+    x = Gen.array_of_func(Gen,n)
+    v = []
+    p = []
+    n1=[]
     otr = [0]
+    num=[]
     x1 = 0
+    a =np.sum(x)/n
 
-    for j in range( m - 1 ):
+
+    for j in range( m ):
         otr.append(x1 + deltax)
-        x1 = otr[j]
-        v.append( (otr[j-1]+otr[j]) / 2 )
-        xone,xtwo = Gen.cut(Gen,otr[j-1], otr[j],x)
+        x1 = otr[j+1]
+
+        xone,xtwo = Gen.cut(Gen,otr[j], otr[j+1],x,n)
+        v.append( (otr[j]+otr[j+1]) / 2 )
+        num.append((xone,xtwo))
         n1.append(xtwo-xone)
         p.append( n1[j] / n )
 
 
 
+    def pint(self):
+        sigma = np.sqrt( self.disp( self ) )
+        p1=[]
+        for i in range(len(self.p)):
+            xone,xtwo=self.num[i]
+            fp = (self.x[int(xone)]-self.expected_value(self))/sigma
+            sp = (self.x[int(xtwo)]-self.expected_value(self))/sigma
+            ffp=Gen.recursive_function(Gen,sp) - Gen.recursive_function(Gen,fp)
+            p1.append(ffp)
+        return p1
+
     def expected_value(self):
         sum=0
         for i in range(len(self.p)):
-            sum=sum+self.p[i]*self.v[i]
-        return sum/2
+            sum=sum+self.n1[i]*self.v[i]
+        return sum/self.n
 
-print (Math1.expected_value(Math1), len(Math1.p), len(Math1.v), Math1.x)
+    def disp(self):
+        sum=0
+        ma=self.expected_value(self)
+        for i in range(len(self.n1)):
+            sum=sum+self.p[i]*(self.v[i]-ma)*(self.v[i]-ma)
+        return sum
+
+
+    def Pearson(self):
+        XI=0
+        p1=self.pint(self)
+        for i in range(len(self.n1)):
+            XI=XI+(self.n1[i]-self.n*p1[i])*(self.n1[i]-self.n*p1[i])/(self.n*p1[i])
+        return XI*self.P
